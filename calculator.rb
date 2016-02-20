fc1519bb604d48cbf2d25cfbc3010a6bf5480155
Ezra Shim <ezrashim@gmail.com>
2016-02-19 19:35:15 -0500

    calculator

--- calculator.rb
+++ calculator.rb
@@ -6,6 +6,24 @@
 
 # The final value will be $10834.71 after 25 years.
 
-# ====================
-# YOUR CODE GOES HERE
-# ====================
+class Calculator
+  attr_accessor :amount, :interest, :years
+  def initialize(amount, interest, years)
+    @amount = amount.to_f
+    @interest = interest.to_f
+    @years = years.to_i
+  end
+
+  def final_value
+    value = (@amount* (1 + @interest/100)**@years).round(2)
+  end
+end
+
+print "What is the amount being invested: "
+amount = gets.chomp
+print "What is the annual interest rate (percentage): "
+interest = gets.chomp
+print "How many years will it accrue interest: "
+years = gets.chomp
+new_calculation = Calculator.new(amount, interest, years)
+puts "The final value will be $#{new_calculation.final_value} after #{new_calculation.years} years."
